c7a56c18ee9565b0d6ad2204bfc383f8ce04991d
Saurabh <saurabhgawande2016@gmail.com>
2022-12-06 15:41:13 +0530

    Fix some bugs related to Textforms

--- src/Components/Textforms.js
+++ src/Components/Textforms.js
@@ -30,6 +30,7 @@ export default function Textforms(props) {
         var text = document.getElementById("my-box");
         text.select();
         navigator.clipboard.writeText(text.value);
+        document.getSelection().removeAllRanges();
         props.showalert("Text Coppied" ,"success");
     }
 
@@ -40,16 +41,16 @@ export default function Textforms(props) {
 
    <h1>{props.header} </h1>
    <div className="mb-3" >
-  <textarea className="form-control" value={text} style={{backgroundColor : props.mode=== 'dark' ?'grey':'white', color:props.mode=== 'dark' ?'white':'black' }} onChange={handleOnClicked} id="my-box" rows="8"></textarea>
+  <textarea className="form-control" value={text} style={{backgroundColor : props.mode=== 'dark' ?'rgb(34 52 58)':'white', color:props.mode=== 'dark' ?'white':'black' }} onChange={handleOnClicked} id="my-box" rows="8"></textarea>
   </div>                                                               
-  <button className="btn btn-primary mx-1" onClick={handleUpClick} >convert to upperCase</button>
-  <button className="btn btn-primary mx-1" onClick={handleLowClick}>convert to lowerCase</button>
-  <button className="btn btn-primary mx-1" onClick={handleCopy}>Copy</button>
-  <button className="btn btn-primary mx-1" onClick={handleClearClick}>clear text</button>
+  <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1" onClick={handleUpClick} >convert to upperCase</button>
+  <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1" onClick={handleLowClick}>convert to lowerCase</button>
+  <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1" onClick={handleCopy}>Copy</button>
+  <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1" onClick={handleClearClick}>clear text</button>
   <div className="container my-3">
     <h2 >Your Text Summary</h2>
-    <p>{text.split(" ").length} words and {text.length} characters</p>
-    <p>{ 0.008*text.split(" ").length} Minutes read</p>
+    <p>{text.split(" ").filter((element)=>{return element.length !== 0}).length} words and {text.length} characters</p>
+    <p>{ 0.008*text.split(" ").filter((element)=>{return element.length !== 0}).length} Minutes read</p>
     <h2>Preview</h2>
     <div>{text}</div>
    <footer>
